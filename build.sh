7ddffeb350b3cb36e01ecdb17808494cb563674f
zack4485-MSFT <72422659+zack4485@users.noreply.github.com>
2024-09-23 16:48:04 -0400

    Update build.sh
    Correcting pathing issues for npm run build and dotnet run commands

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet build .
-cd MyApp
 npm install -D tailwindcss
-npm run build
-./dotnet/dotnet run --AppTasks=prerender --environment Production --BaseUrl "https://thenerdery.io"
+npm --prefix ./MyApp run build
+./dotnet/dotnet run --AppTasks=prerender --environment Production --BaseUrl "https://thenerdery.io" --project ./MyApp
