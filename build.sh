4388ab17cfa21c07641f721ce8a52aa52ccc38e2
zack4485-MSFT <72422659+zack4485@users.noreply.github.com>
2024-09-23 17:15:56 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet build .
-pwd
-ls -al
 npm install -D tailwindcss
-pwd
-ls -al
 npm --prefix ./MyApp run build
-/opt/buildhome/repo/dotnet/dotnet run --AppTasks=prerender --environment Production --BaseUrl "https://thenerdery.io" --project /opt/buildhome/repo/MyApp
+./dotnet/dotnet run --AppTasks=prerender --environment Production --BaseUrl "https://thenerdery.io" --project ./MyApp
