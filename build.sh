ac72948f1a8385228ca216e56ef95dd957214206
zack4485 <zjohnson@batcavity.com>
2024-09-25 01:42:06 -0400

    Syncing changes to build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
+./dotnet/dotnet build .
+npm install -D tailwindcss
+npm --prefix ./MyApp run build
+./dotnet/dotnet run --AppTasks=prerender --environment Production --BaseUrl "https://thenerdery.io" --project ./MyApp
